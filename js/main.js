94e512bc8b7ffca8f7da0e70d22a8679c6d7c489
NoN4ame <Keir548@yandex.ru>
2021-03-24 21:21:08 +0300

    Мелкие правки в стилях

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 const menu = {
     init() {
         this.showMenu()
+
     },
     showMenu() {
         let nav = document.querySelectorAll('.icon')
@@ -13,7 +14,35 @@ const menu = {
                     item.classList.add('closeIcon')
                 }
             })
+            this.renderNavigate()
         })
+
     },
+    navigateTemplate() {
+        document.querySelector('.menu-icon').insertAdjacentHTML('afterend',
+            `<section class="nav">
+                    <ul>
+                    <li>Портфолио</li>
+                    <li>Вилка цен</li>
+                    <li>Бриф</li>
+                    <li>Контакты</li>
+                    </ul>
+                  </section>`)
+    },
+    renderNavigate() {
+        if (document.querySelector('.icon').classList.contains('closeIcon') === true) {
+            this.navigateTemplate()
+            setTimeout(this.openNavigateAnimate, 50)
+        } else {
+            document.querySelector('.nav').classList.remove('active')
+            setTimeout(this.removeNavigate, 50)
+        }
+    },
+    openNavigateAnimate() {
+        document.querySelector('.nav').classList.add('active')
+    },
+    removeNavigate() {
+        document.querySelector('.nav').remove()
+    }
 }
 menu.init()
